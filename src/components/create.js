abc391aa0beb4dcf997ca23f959c8444662881b2
VEDA GREER - STUDENT <G00388406@atu.ie>
2022-10-17 16:45:41 +0100

    first commit

--- src/components/create.js
+++ src/components/create.js
@@ -1,11 +1,91 @@
 import React from "react";
 
-export class Create extends React.Component{
+export class Create extends React.Component {
 
-    render(){
-        return(
+    constructor(){
+        
+        //invoking parents constructor
+        super();
+
+        //binding different events
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+        this.onChangeBookCover = this.onChangeBookCover.bind(this);
+        this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
+
+        //updating the states and making them equal to empty strings
+        this.state = {
+            title:'',
+            cover:'',
+            author:''
+        }
+    }
+
+    //method for submitting the form. Takes an event as an argument
+    handleSubmit(e){
+        e.preventDefault();
+        console.log(`button cliked 
+        ${this.state.title},
+        ${this.state.cover},
+        ${this.state.author}`);
+    }
+
+    //invoked when value is changed to update the state
+    onChangeBookTitle(e){
+        this.setState({
+            title:e.target.value
+        })
+    }
+
+    //invoked when value is changed to update the state
+    onChangeBookCover(e){
+        this.setState({
+            cover:e.target.value
+        })
+    }
+
+    //invoked when value is changed to update the state
+    onChangeBookAuthor(e){
+        this.setState({
+            author:e.target.value
+        })
+    }
+
+    //creating a form to display and alter information
+    render() {
+        return (
             <div>
-                <h3>Hello from my Create component!</h3>
+                <form onSubmit={this.handleSubmit}>
+
+                    <div className="form-title">
+                        <label>Add Book Title: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.title}
+                            onChange={this.onChangeBookTitle}
+                        />
+                    </div>
+
+                    <div className="form-cover">
+                        <label>Add Book Cover: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.cover}
+                            onChange={this.onChangeBookCover}
+                        />
+                    </div>
+
+                    <div className="form-author">
+                        <label>Add Book Author: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.URL}
+                            onChange={this.onChangeBookAuthor}
+                        />
+                    </div>
+
+                    <input type="submit" value="Add Book" />
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,49 +1,23 @@
 import React from "react";
 import { Books } from "./books";
+import axios from "axios";
 
 export class Read extends React.Component{
 
+    //importing information using a url of data and assigning it to the books state
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
+        .then((response)=>{
+            this.setState({books:response.data})
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     //importing information about books through arrays of objects
     state ={
-        books: [
-            {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-            },
-            {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-            "Kyle Banker",
-            "Peter Bakkum",
-            "Tim Hawkins",
-            "Shaun Verch",
-            "Douglas Garrett"
-            ],
-            "categories": []
-            },
-            {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-            }
-            ]
-            
+        books: []   
     }
 
     //rendering the read component information and displaying the book information from books.js
